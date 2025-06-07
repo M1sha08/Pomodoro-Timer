33105ccbd72e156fcca0c0dcc87657796150e653
M1sha08 <mishadiasamidze2008@gmail.com>
2025-06-07 12:14:43 +0200

    Added To-Do List, Theme-Toggle, ReadMe and fixed few bugs

---
+++ readme.md
@@ -0,0 +1,27 @@
+## Pomodoro Timer App
+
+A simple pomodoro timer app with an integrated
+to-do list.
+
+## Features
+
+# Pomodoro Timer
+
+Customizable Short and Long Breaks
+Start/Pause/Stop Control Buttons
+
+# To-Do List
+
+Add new tasks
+Check task as complete
+Delete tasks
+
+# Additional Features
+
+Light/Dark theme toggle
+
+# Future Goals
+
+Add LocalStorage to save tasks
+Add feature to track pomodoro session types
+Add task editing feature
--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// THEME-TOGGLE BUTTON
+const themeToggleBtn = document.getElementById("theme-toggle");
+
+themeToggleBtn.addEventListener("click", () => {
+  const dataTheme = document.documentElement.getAttribute("data-theme");
+  
+  if(dataTheme === "dark"){
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+  else{
+    document.documentElement.setAttribute("data-theme", "dark");
+  };
+});
+
 // POMODORO TIMER
 const timeDisplay = document.querySelector(".time");
 
@@ -20,8 +34,8 @@ function formatTime(minutes){
   return `${mins}:00`;
 };
 
-/* Time button controls */
 
+// TIME-CONTROL BUTTONS
 timeControlButtons.forEach(button => {
   button.addEventListener('click', () => {
     
@@ -85,22 +99,110 @@ stopBtn.addEventListener('click', () => {
 
 /* BG Color of Body Switches on hovering the buttons */
 startBtn.addEventListener('mouseenter', () =>{
- document.body.style.backgroundColor = "hsla(120, 100%, 40%, 0.3)";
+ document.body.style.backgroundColor = "hsla(120, 100%, 20%, 0.5)";
 });
 startBtn.addEventListener('mouseleave', () =>{
  document.body.style.backgroundColor = "var(--light-color)";
 });
 
 pauseBtn.addEventListener('mouseenter', () =>{
- document.body.style.backgroundColor = "hsla(60, 100%, 40%, 0.3)";
+ document.body.style.backgroundColor = "hsla(60, 100%, 20%, 0.5)";
 });
 pauseBtn.addEventListener('mouseleave', () =>{
  document.body.style.backgroundColor = "var(--light-color)";
 });
 
 stopBtn.addEventListener('mouseenter', () =>{
- document.body.style.backgroundColor = "hsla(0, 100%, 40%, 0.3)";
+ document.body.style.backgroundColor = "hsla(0, 100%, 20%, 0.5)";
 });
 stopBtn.addEventListener('mouseleave', () =>{
  document.body.style.backgroundColor = "var(--light-color)";
+});
+
+
+//TASKS-CONTENT
+const addTaskInput = document.getElementById("add-task-input");
+const addTaskBtn = document.getElementById("add-task-button");
+
+
+const maxInputChars = 50;
+const maxTasks = 10;
+
+const noTasksYetText = document.querySelector(".no-tasks-text");
+
+
+addTaskBtn.addEventListener('click', () => {
+  const addTaskInputValue = addTaskInput.value.trim();
+
+  
+  if (addTaskInputValue.length === 0) {
+    addTaskInput.setAttribute("placeholder",
+    "Input can't be empty!");
+    return;
+  } 
+  else if (addTaskInputValue.length > maxInputChars) {
+    addTaskInput.value = "";
+    addTaskInput.setAttribute(
+      "placeholder",
+      `Input can't be longer than a ${maxInputChars} characters!`
+    );
+    return;
+  }
+  else{
+    const tasksCount = document.querySelectorAll(".task").length;
+
+    if(tasksCount >= maxTasks){
+      addTaskInput.value = "";
+      addTaskInput.setAttribute("placeholder", 
+        `You can't have more then a ${maxTasks} Tasks`
+      )
+      return;
+    };  
+  }
+
+
+  const taskContainer = document.querySelector(".task-container");
+  const task = document.createElement('div');
+  const taskText = document.createElement('p');
+  const taskActions = document.createElement("div");
+  const taskCheckbox = document.createElement('input');
+  const taskDelete = document.createElement('button');
+
+  
+  task.classList.add('task');
+  taskText.classList.add("task-text");
+  taskActions.classList.add("task-actions");
+  taskCheckbox.classList.add("task-checkbox");
+  taskDelete.classList.add("task-delete");
+
+
+  taskText.textContent = addTaskInputValue;
+  taskCheckbox.type = "checkbox";
+  taskDelete.textContent = "X";
+
+  taskContainer.appendChild(task);
+  task.appendChild(taskText);
+  task.appendChild(taskActions);
+  taskActions.appendChild(taskCheckbox);
+  taskActions.appendChild(taskDelete);
+
+  addTaskInput.value = "";
+  noTasksYetText.style.display = "none";
+
+  taskCheckbox.addEventListener("change", () => {
+    taskText.classList.toggle("checked", taskCheckbox.checked);
+  });
+
+  taskDelete.addEventListener("click", () => {
+    task.remove();
+    if(document.querySelectorAll(".task").length === 0){
+      noTasksYetText.style.display = "block";
+    };
+  });
+});
+
+addTaskInput.addEventListener("keydown", (e) => {
+  if(e.key === "Enter"){
+    addTaskBtn.click();
+  };
 });
